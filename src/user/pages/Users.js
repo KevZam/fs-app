77cb83a2a17d45f5ab1b27080f026a94f1e9824a
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-08-03 15:32:06 -0700

    added styling for card

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
-  return <h2>Users Works!</h2>
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Robert Ford',
+      image: 'https://www.thewrap.com/wp-content/uploads/2018/06/Westworld-Ford-Anthony-Hopkins.jpg',
+      places: 3
+    }];
+
+  return <UsersList items={USERS} />
 }
 
 export default Users;
