e0fa622f4c3434e21ad9711fc7a98c43916d38a0
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-09-09 09:30:15 -0700

    configured DELETE, PATCH requests for front end

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useState, useContext } from 'react';
 
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import Modal from '../../shared/components/UIElements/Modal';
 import Map from '../../shared/components/UIElements/Map';
 import Card from '../../shared/components/UIElements/Card'
 import Button from '../../shared/components/FormElements/Button'
 import { AuthContext } from '../../shared/context/auth-context';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 import './PlaceItem.css'
 
 const PlaceItem = props => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const auth = useContext(AuthContext);
 
   const [showMap, setShowMap] = useState(false);
@@ -26,13 +30,17 @@ const PlaceItem = props => {
     setShowConfirmModal(false)
   }
 
-  const confirmDeleteHandler = () => {
+  const confirmDeleteHandler = async () => {
     setShowConfirmModal(false);
-    console.log("DELETING...")
+    try {
+      await sendRequest(`http://localhost:5000/api/places/${props.id}`, 'DELETE');
+      props.onDelete(props.id)
+    } catch (err) { }
   }
 
   return (
     <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
       <Modal
         show={showMap}
         onCancel={closeMapHandler}
@@ -59,6 +67,7 @@ const PlaceItem = props => {
       </Modal>
       <li className="place-item">
         <Card className="place-item__content">
+          {isLoading && <LoadingSpinner asOverlay />}
           <div className="place=item__image">
             <img src={props.imageUrl} alt={props.title} />
           </div>
