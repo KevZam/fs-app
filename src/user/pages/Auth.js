722b5a2fc01d871e5bab042ce483f11fccd78d36
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-09-09 10:04:06 -0700

    added ImageUpload component

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -13,6 +13,7 @@ import { AuthContext } from '../../shared/context/auth-context'
 import ErrorModal from '../../shared/components/UIElements/ErrorModal'
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner'
 import { useHttpClient } from '../../shared/hooks/http-hook';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
 import './Auth.css';
 
 const Auth = () => {
@@ -114,6 +115,7 @@ const Auth = () => {
               onInput={inputHandler}
             />
           )}
+          {!isLoginMode && <ImageUpload id="image" center />}
           <Input
             element="input"
             id="email"
