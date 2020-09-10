71c1ff8f8f99625ef7a00e5cd523bd2a2234971a
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-09-10 15:25:54 -0700

    test solution

--- src/shared/components/UIElements/ErrorModal.js
+++ src/shared/components/UIElements/ErrorModal.js
@@ -4,6 +4,7 @@ import Modal from './Modal';
 import Button from '../FormElements/Button';
 
 const ErrorModal = props => {
+  console.log(props)
   return (
     <Modal
       onCancel={props.onClear}
