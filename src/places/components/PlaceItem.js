29118e501c47f2a1e6f49825440f935f217ec3b6
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-09-10 11:04:13 -0700

    prepared for deployment

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -33,7 +33,7 @@ const PlaceItem = props => {
   const confirmDeleteHandler = async () => {
     setShowConfirmModal(false);
     try {
-      await sendRequest(`http://localhost:5000/api/places/${props.id}`, 'DELETE', null, {
+      await sendRequest(process.env.REACT_APP_BACKEND_URL + `/places/${props.id}`, 'DELETE', null, {
         Authorization: 'Bearer ' + auth.token
       });
       props.onDelete(props.id)
@@ -71,7 +71,7 @@ const PlaceItem = props => {
         <Card className="place-item__content">
           {isLoading && <LoadingSpinner asOverlay />}
           <div className="place=item__image">
-            <img src={`http://localhost:5000/${props.imageUrl}`} alt={props.title} />
+            <img src={`${process.env.REACT_APP_ASSET_URL}/${props.imageUrl}`} alt={props.title} />
           </div>
           <div className="place-item__info">
             <h2>{props.title}</h2>
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -70,7 +70,7 @@ const Auth = () => {
 
     if (isLoginMode) {
       try {
-        const responseData = await sendRequest('http://localhost:5000/api/users/login', 'POST', JSON.stringify({
+        const responseData = await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/users/login`, 'POST', JSON.stringify({
           email: formState.inputs.email.value,
           password: formState.inputs.password.value
         }),
@@ -89,7 +89,7 @@ const Auth = () => {
         formData.append('email', formState.inputs.email.value);
         formData.append('password', formState.inputs.password.value);
         formData.append('image', formState.inputs.image.value);
-        const responseData = await sendRequest('http://localhost:5000/api/users/signup', 'POST',
+        const responseData = await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/users/signup`, 'POST',
           formData,
         );
 
