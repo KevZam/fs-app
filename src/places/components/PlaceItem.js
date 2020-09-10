a342485d312e1ab723d123304cd3c2806d8f7556
Kevan Zameni <39892438+KevZam@users.noreply.github.com>
2020-09-10 16:49:36 -0700

    trying fix

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -33,7 +33,7 @@ const PlaceItem = props => {
   const confirmDeleteHandler = async () => {
     setShowConfirmModal(false);
     try {
-      await sendRequest(process.env.REACT_APP_BACKEND_URL + `/places/${props.id}`, 'DELETE', null, {
+      await sendRequest(`${process.env.REACT_APP_BACKEND_URL}/places/${props.id}`, 'DELETE', null, {
         Authorization: 'Bearer ' + auth.token
       });
       props.onDelete(props.id)
